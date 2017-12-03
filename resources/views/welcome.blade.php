18d93fc0ab9fc2d7dd7b88b8f7290a5d396a5c6e
João Victor de Lemos Nery <joao.victor.lemos.nery@gmail.com>
2017-12-02 23:11:31 -0200

    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cafeina
+                    Cafeina teste
                 </div>
 
                 <div class="links">
